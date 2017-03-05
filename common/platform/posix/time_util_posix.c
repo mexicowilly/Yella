9d7117c6b8eafbc41ec7b9ee18e678b478803cd3
Will Mason <willchido@gmail.com>
2017-03-05 10:48:56 -0600

    Added some more files for spool testing

---
+++ common/platform/posix/time_util_posix.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016 Will Mason
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "common/time_util.h"
+#include <sys/time.h>
+#include <stddef.h>
+
+uintmax_t yella_millis_since_epoch(void)
+{
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+}
