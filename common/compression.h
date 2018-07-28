e00cb58f1ae679717f184b6f9e44fb42e2a44df5
Will Mason <willchido@gmail.com>
2018-07-28 09:20:24 -0700

    Fix #8. Clang leak checker enabled

--- common/compression.h
+++ common/compression.h
@@ -1,10 +1,11 @@
 #ifndef YELLA_COMPRESSION_H__
 #define YELLA_COMPRESSION_H__
 
+#include "export.h"
 #include <stdint.h>
 #include <stddef.h>
 
-uint8_t* yella_lz4_compress(const uint8_t* const bytes, size_t* size);
-uint8_t* yella_lz4_decompress(const uint8_t* const bytes, size_t* size);
+YELLA_EXPORT uint8_t* yella_lz4_compress(const uint8_t* const bytes, size_t* size);
+YELLA_EXPORT uint8_t* yella_lz4_decompress(const uint8_t* const bytes, size_t* size);
 
 #endif
