543871bcc7dda5e4686fe17071fdb9f4095d015d
Will Mason <willchido@gmail.com>
2018-12-05 18:27:38 -0800

    Job queue complete

--- plugin/file/job_queue.c
+++ plugin/file/job_queue.c
@@ -18,12 +18,47 @@ struct job_queue
     size_t sz;
     yella_mutex* guard;
     yella_condition_variable* cond;
+    yella_thread* runner;
+    bool should_stop;
 };
 
 #define JOB_COMPARATOR(lhs, rhs) (u_strcmp(lhs->jb->config_name, rhs->jb->config_name))
 
 SGLIB_DEFINE_DL_LIST_PROTOTYPES(queue, JOB_COMPARATOR, previous, next);
 SGLIB_DEFINE_DL_LIST_FUNCTIONS(queue, JOB_COMPARATOR, previous, next);
+
+static void job_queue_main(void* udata)
+{
+    job_queue* jq;
+    queue* front;
+
+    jq = (job_queue*)udata;
+    while (true)
+    {
+        yella_lock_mutex(jq->guard);
+        while (!jq->should_stop && jq->sz == 0)
+            yella_wait_milliseconds_for_condition_variable(jq->cond, jq->guard, 250);
+        if (jq->should_stop)
+        {
+            yella_unlock_mutex(jq->guard);
+            break;
+        }
+        if (jq->sz == 0)
+        {
+            yella_unlock_mutex(jq->guard);
+        }
+        else
+        {
+            front = sglib_queue_get_first(jq->q);
+            sglib_queue_delete(&jq->q, front);
+            --jq->sz;
+            yella_unlock_mutex(jq->guard);
+            run_job(front->jb);
+            destroy_job(front->jb);
+            free(front);
+        }
+    }
+}
 
 job_queue* create_job_queue(void)
 {
@@ -32,6 +67,7 @@ job_queue* create_job_queue(void)
     result = calloc(1, sizeof(job_queue));
     result->guard = yella_create_mutex();
     result->cond = yella_create_condition_variable();
+    result->runner = yella_create_thread(job_queue_main, result);
     return result;
 }
 
@@ -40,6 +76,11 @@ void destroy_job_queue(job_queue* jq)
     struct sglib_queue_iterator itor;
     queue* q;
 
+    yella_lock_mutex(jq->guard);
+    jq->should_stop = true;
+    yella_unlock_mutex(jq->guard);
+    yella_join_thread(jq->runner);
+    yella_destroy_thread(jq->runner);
     yella_destroy_condition_variable(jq->cond);
     yella_destroy_mutex(jq->guard);
     for (q = sglib_queue_it_init(&itor, jq->q);
@@ -68,9 +109,4 @@ void push_job_queue(job_queue* jq, job* jb)
     if (jq->sz == 1)
         yella_signal_condition_variable(jq->cond);
     yella_unlock_mutex(jq->guard);
-}
-
-void run_job_queue(job_queue* jq)
-{
-
 }
--- plugin/file/job_queue.h
+++ plugin/file/job_queue.h
@@ -8,6 +8,5 @@ typedef struct job_queue job_queue;
 job_queue* create_job_queue(void);
 void destroy_job_queue(job_queue* jq);
 void push_job_queue(job_queue* jq, job* jb);
-void run_job_queue(job_queue* jq);
 
 #endif
