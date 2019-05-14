5dc36bc17f1a07020802ea5a3c7930d2c8b4f5de
Will Mason <willchido@gmail.com>
2019-05-14 07:43:17 -0700

    Accumulator latency test

--- test/plugin/file/accumulator_test.c
+++ test/plugin/file/accumulator_test.c
@@ -72,6 +72,31 @@ static int tear_down(void** arg)
     return 0;
 }
 
+static void latency(void** arg)
+{
+    test_data* td;
+    element* elem;
+    attribute* attr;
+
+    td = *arg;
+    elem = create_element(u"jumpy lumpy");
+    attr = malloc(sizeof(attribute));
+    attr->type = ATTR_TYPE_FILE_TYPE;
+    attr->value.integer = YELLA_FILE_TYPE_REGULAR;
+    add_element_attribute(elem, attr);
+    add_accumulator_message(td->acc,
+                            u"funky",
+                            u"config me",
+                            element_name(elem),
+                            elem,
+                            yella_fb_file_condition_CHANGED);
+    destroy_element(elem);
+    yella_sleep_this_thread(*yella_settings_get_uint(u"file", u"send-latency-seconds") * 1000 * 3);
+    yella_lock_mutex(td->guard);
+    assert_int_equal(yella_ptr_vector_size(td->recs), 1);
+    yella_unlock_mutex(td->guard);
+}
+
 static void size(void** arg)
 {
     test_data* td;
@@ -84,9 +109,9 @@ static void size(void** arg)
     size_t count;
 
     td = *arg;
-    /* Add 1% extra because the message will only be sent once the max size is exceeded */
+    /* Add 2% extra because the message will only be sent once the max size is exceeded */
     max_sz = *yella_settings_get_byte_size(u"agent", u"max-message-size");
-    max_sz += max_sz * .01;
+    max_sz += max_sz * .02;
     latency_time = ucal_getNow() + (*yella_settings_get_uint(u"file", u"send-latency-seconds") * 1000);
     elem = create_element(u"jumpy lumpy");
     attr = malloc(sizeof(attribute));
@@ -100,28 +125,33 @@ static void size(void** arg)
                                 u"funky",
                                 u"config me",
                                 element_name(elem),
-                                elem, yella_fb_file_condition_CHANGED);
+                                elem,
+                                yella_fb_file_condition_CHANGED);
         ++count;
         yella_lock_mutex(td->guard);
         done = yella_ptr_vector_size(td->recs) == 1;
         yella_unlock_mutex(td->guard);
     } while (!done);
+    destroy_element(elem);
     CHUCHO_C_INFO(td->lgr, "Added %zu messages to accumulator", count);
     assert_true(ucal_getNow() <= latency_time);
+    yella_lock_mutex(td->guard);
     rec = yella_ptr_vector_at(td->recs, 0);
     assert_true(rec->sz < max_sz);
+    yella_unlock_mutex(td->guard);
 }
 
 int main()
 {
     const struct CMUnitTest tests[] =
     {
-        cmocka_unit_test_setup_teardown(size, set_up, tear_down)
+        cmocka_unit_test_setup_teardown(size, set_up, tear_down),
+        cmocka_unit_test_setup_teardown(latency, set_up, tear_down)
     };
 
     yella_load_settings_doc();
     yella_destroy_settings_doc();
     yella_settings_set_byte_size(u"agent", u"max-message-size", u"1MB");
-    yella_settings_set_uint(u"file", u"send-latency-seconds", 10);
+    yella_settings_set_uint(u"file", u"send-latency-seconds", 1);
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
